d9bf43ad9e189e44077ba97b64c8eb8c63bfd565
junjchen <chuckjunjchen@gmail.com>
2018-08-07 21:12:09 +0100

    aggregate birth date field

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 import csv
 import json
 import requests
-from flask import (Flask, request, redirect, send_from_directory, render_template)
+from flask import (Flask, request, redirect, render_template)
 
 app = Flask(__name__)
 
+def middleware_factory():
+    def aggregate_dob(data):
+        for row in data:
+            if 'birth.year' in row and 'birth.month' in row and 'birth.day' in row:
+                by = row['birth.year']
+                bm = row['birth.month']
+                bd = row['birth.day']
+                row['birth'] = {
+                    'year': by,
+                    'month': bm,
+                    'day': bd
+                }
+                del row['birth.year']
+                del row['birth.month']
+                del row['birth.day']
+        return data
+    return {
+        'aggregate_dob': aggregate_dob
+    }
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    middlewares = middleware_factory()
     if request.method == 'GET':
-        return send_from_directory('static', 'index.html')
+        return render_template('index.html', middlewares=middlewares)
     else:
         csvfile = request.files['file']
         text = [line.decode('utf-8') for line in csvfile]
         data = []
         for row in csv.DictReader(text, quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True):
             data.append(row)
-        
+
+        if 'middlewares' in request.form:
+            for x in request.form.getlist('middlewares'):
+                data = middlewares[x](data)
+
         targeturl = request.form['target']
         res = requests.post(targeturl, json=data)
         return render_template('res.html', res=res, str=json.dumps(data, indent=4, sort_keys=True))
