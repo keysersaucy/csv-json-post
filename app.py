65fb0e0cf1585ccf93e6e43623c09e83ecdf1cb8
keysersaucy <42117501+keysersaucy@users.noreply.github.com>
2018-08-13 18:57:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def index():
     else:
         csvfile = request.files['file']
         text = [line.decode('utf-8') for line in csvfile]
-        data = []
+        data = [text1, text2]
         for row in csv.DictReader(text, quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True):
             data.append(row)
 
